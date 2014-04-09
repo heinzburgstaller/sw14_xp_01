a17fb2d3f6463d58b77a1d898ef965a475dcb737
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2014-04-09 13:16:59 +0200

    [CB,HB]{other}Added Contact Test/Code

--- HoiServer/src/at/tugraz/sw/hoi/HoiServerServlet.java
+++ HoiServer/src/at/tugraz/sw/hoi/HoiServerServlet.java
@@ -5,9 +5,15 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class HoiServerServlet extends HttpServlet {
+
+	public static final String PARAM_NUMBER_1 = "number1";
+	public static final String PARAM_NUMBER_2 = "number2";
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		int number1 = Integer.parseInt(req.getParameter(PARAM_NUMBER_1));
+		int number2 = Integer.parseInt(req.getParameter(PARAM_NUMBER_2));
+		resp.getWriter().print(Integer.toString(number1 + number2));
 	}
 }
