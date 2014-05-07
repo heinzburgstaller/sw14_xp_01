f424b0dd681f23cb72051053e9e96556326a5216
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2014-05-07 11:34:48 +0200

    [HB,CB]{test before code} Added Test ChatServledSuccess

--- HoiServer/test/at/tugraz/sw/hoi/server/TestChatServlet.java
+++ HoiServer/test/at/tugraz/sw/hoi/server/TestChatServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,6 +13,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import at.tugraz.sw.hoi.model.Contact;
+import at.tugraz.sw.hoi.model.EMFService;
 
 import com.google.appengine.api.datastore.dev.LocalDatastoreService;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
@@ -28,6 +32,18 @@ public class TestChatServlet {
 		LocalDatastoreService dsService = (LocalDatastoreService) helper
 				.getLocalService(LocalDatastoreService.PACKAGE);
 		dsService.setNoStorage(true);
+
+		EntityManager em = EMFService.get().createEntityManager();
+
+		Contact to = new Contact("sender@gmail.com", "sender");
+		Contact from = new Contact("receiver@gmail.com", "receiver");
+
+		em.persist(to);
+		em.close();
+
+		em = EMFService.get().createEntityManager();
+		em.persist(from);
+		em.close();
 	}
 
 	@After
@@ -54,9 +70,28 @@ public class TestChatServlet {
 		writer.flush();
 		writer.close();
 
-		Assert.assertTrue(sr.toString().equals(Configuration.FAILURE));
-
+		Assert.assertTrue(sr.toString().startsWith(Configuration.FAILURE));
 	}
-	
-	
+
+	@Test
+	public void testChatSuccess() throws IOException {
+		HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+		HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+		StringWriter sr = new StringWriter();
+		PrintWriter writer = new PrintWriter(sr);
+		Mockito.when(response.getWriter()).thenReturn(writer);
+
+		Mockito.when(request.getParameter(Configuration.FROM)).thenReturn(
+				"sender@gmail.com");
+		Mockito.when(request.getParameter(Configuration.TO)).thenReturn(
+				"receiver@gmail.com");
+		Mockito.when(request.getParameter(Configuration.MSG)).thenReturn(
+				"This is a message.");
+
+		new ChatServlet().doPost(request, response);
+		writer.flush();
+		writer.close();
+		System.out.println(sr.toString());
+		Assert.assertTrue(sr.toString().startsWith(Configuration.SUCCESS));
+	}
 }
