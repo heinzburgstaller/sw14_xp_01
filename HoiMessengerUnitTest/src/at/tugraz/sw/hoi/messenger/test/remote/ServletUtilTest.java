70309e61e9d740294edf383551f0749e778114f3
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2014-05-14 09:49:42 +0200

    [HB,CB]{test before code} New test class

--- HoiMessengerUnitTest/src/at/tugraz/sw/hoi/messenger/test/remote/ServletUtilTest.java
+++ HoiMessengerUnitTest/src/at/tugraz/sw/hoi/messenger/test/remote/ServletUtilTest.java
@@ -1,5 +1,18 @@
 package at.tugraz.sw.hoi.messenger.test.remote;
 
-public class ServletUtilTest {
+import at.tugraz.sw.hoi.messenger.remote.ServletResponse;
+import at.tugraz.sw.hoi.messenger.remote.ServletResponse.Status;
+import at.tugraz.sw.hoi.messenger.remote.ServletUtil;
+import junit.framework.TestCase;
+
+public class ServletUtilTest extends TestCase {
+
+	private static final String EMAIL = "heinz.burgstaller@gmail.com";
+	private static final String REG_ID = "APA91bHlMWAR8404lQvJsFMxDxNn6JjdTYGM7NOVAP3ENHqaYfGfmDatew4G7i03U_QUc5YlMZ1VJl3ZemXVyTHU3fbxHVDS_iIw6mU8GgsiQRC-33sBaMsr97RFqfkgdxFMmfAw5YZf2NA6qXZBH2K09dgWCTxv3A";
+
+	public void testRegister() {
+		ServletResponse response = ServletUtil.register(EMAIL, REG_ID);
+		assertTrue(Status.SUCCESS.equals(response.getStatus()));
+	}
 
 }
