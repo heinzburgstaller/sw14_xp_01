eb2a425d9aef43e689184b233b2f998c47e62e57
Clemens Binder <binder.clemens@gmail.com>
2014-05-21 16:12:36 +0200

    [CB,HB]{bugfix} Unregister GcmUtil

--- HoiMessenger/src/at/tugraz/sw/hoi/messenger/remote/GcmUtil.java
+++ HoiMessenger/src/at/tugraz/sw/hoi/messenger/remote/GcmUtil.java
@@ -115,41 +115,17 @@ public class GcmUtil {
     unRegisterTask = new AsyncTask<Void, Void, Boolean>() {
       @Override
       protected Boolean doInBackground(Void... params) {
-        long backoff = BACKOFF_MILLI_SECONDS + random.nextInt(1000);
+        // long backoff = BACKOFF_MILLI_SECONDS + random.nextInt(1000);
         for (int i = 1; i <= MAX_ATTEMPTS; i++) {
-          // Log.d(TAG, "Attempt #" + i + " to register");
-          try {
-            if (gcm == null) {
-              gcm = GoogleCloudMessaging.getInstance(ctx);
-            }
-            String regid = gcm.register(Configuration.SENDER_ID);
+          // You should send the registration ID to your server over HTTP,
+          // so it can use GCM/HTTP or CCS to send messages to your app.
+          ServletResponse response = ServletUtil.unregister(getPreferredEmail());
 
-            // You should send the registration ID to your server over HTTP,
-            // so it can use GCM/HTTP or CCS to send messages to your app.
-            ServletResponse response = ServletUtil.unregister(getPreferredEmail());
-
-            if (response.getStatus() == ServletResponse.Status.FAILURE) {
-              return Boolean.FALSE;
-            }
-
-            return Boolean.TRUE;
-
-          } catch (IOException ex) {
-            // Log.e(TAG, "Failed to register on attempt " + i + ":" + ex);
-            if (i == MAX_ATTEMPTS) {
-              break;
-            }
-            try {
-              // Log.d(TAG, "Sleeping for " + backoff + " ms before retry");
-              Thread.sleep(backoff);
-            } catch (InterruptedException e1) {
-              // Activity finished before we complete - exit.
-              // Log.d(TAG, "Thread interrupted: abort remaining retries!");
-              Thread.currentThread().interrupt();
-            }
-            // increase backoff exponentially
-            backoff *= 2;
+          if (response.getStatus() == ServletResponse.Status.FAILURE) {
+            continue;
           }
+
+          return Boolean.TRUE;
         }
         return Boolean.FALSE;
       }
@@ -241,6 +217,9 @@ public class GcmUtil {
   public void cleanup() {
     if (registrationTask != null) {
       registrationTask.cancel(true);
+    }
+    if (unRegisterTask != null) {
+      unRegisterTask.cancel(true);
     }
     if (gcm != null) {
       gcm.close();
