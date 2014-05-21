da3c5959b5aa9d924dd11127d4c3b03d045f7c7b
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2014-05-21 09:12:32 +0200

    [HB,CB]{test after code} Added test for registration

--- HoiMessengerUnitTest/src/at/tugraz/sw/hoi/messenger/test/remote/GcmUtilTest.java
+++ HoiMessengerUnitTest/src/at/tugraz/sw/hoi/messenger/test/remote/GcmUtilTest.java
@@ -4,30 +4,35 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
-import android.test.InstrumentationTestCase;
+import android.test.AndroidTestCase;
 import at.tugraz.sw.hoi.messenger.remote.Configuration;
 import at.tugraz.sw.hoi.messenger.remote.GcmUtil;
 
-public class GcmUtilTest extends InstrumentationTestCase {
+public class GcmUtilTest extends AndroidTestCase {
 
-	public Context context;
+	private Context context;
+	private SharedPreferences prefs;
+
 	@Override
 	protected void setUp() throws Exception {
-		// TODO Auto-generated method stub
 		super.setUp();
-		context = getInstrumentation().getContext();
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		context = getContext();
+		prefs = PreferenceManager.getDefaultSharedPreferences(context);
 		Editor editor = prefs.edit();
-		
+
 		editor.putString(Configuration.PROPERTY_REG_ID, "");
-		editor.putString(Configuration.CHAT_EMAIL_ID, "heinz.burgstaller@gmail.com");
+		editor.putString(Configuration.CHAT_EMAIL_ID,
+				"heinz.burgstaller@gmail.com");
 		editor.commit();
-		
-		
 	}
+
 	public void testGetPrefered() {
 		GcmUtil gcmutil = new GcmUtil(context);
-		assertEquals("heinz.burgstaller@gmail.com",gcmutil.getPreferredEmail());
-		
+		String regId = prefs.getString(Configuration.PROPERTY_REG_ID, "Fail");
+
+		android.util.Log.d("JUnit", regId);
+		assertEquals("heinz.burgstaller@gmail.com", gcmutil.getPreferredEmail());
+		assertNotSame("Fail", regId);
+		assertEquals("heinz.burgstaller@gmail.com", gcmutil.getPreferredEmail());
 	}
 }
