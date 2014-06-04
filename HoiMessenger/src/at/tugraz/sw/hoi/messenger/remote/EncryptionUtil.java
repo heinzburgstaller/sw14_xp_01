19baa58bef59b11f9c1a133be05debabb82dd209
Clemens Binder <binder.clemens@gmail.com>
2014-06-04 08:25:54 +0200

    [CB,HB]{code} Add method EncryptionUtil

--- HoiMessenger/src/at/tugraz/sw/hoi/messenger/remote/EncryptionUtil.java
+++ HoiMessenger/src/at/tugraz/sw/hoi/messenger/remote/EncryptionUtil.java
@@ -1,10 +1,16 @@
 package at.tugraz.sw.hoi.messenger.remote;
 
 import java.security.Key;
+import java.security.KeyFactory;
+import java.security.spec.X509EncodedKeySpec;
 
+import javax.crypto.Cipher;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Base64;
 
 public class EncryptionUtil {
   private Context ctx;
@@ -20,5 +26,31 @@ public class EncryptionUtil {
         || prefs.getString(Configuration.PUBLIC_KEY, "").equals("")) {
 
     }
+  }
+
+  @SuppressLint("TrulyRandom")
+  public static String encrypt(String input, String b64PubKey) throws Exception {
+    byte[] inArr = input.getBytes();
+    byte[] pubKey = Base64.decode(b64PubKey, Base64.DEFAULT);
+    Key publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(pubKey));
+    Cipher c = Cipher.getInstance("RSA");
+    c.init(Cipher.ENCRYPT_MODE, publicKey);
+    byte[] decodedBytes = c.doFinal(inArr);
+    String output = new String(decodedBytes, "UTF8");
+
+    return output;
+
+  }
+
+  public static String decrypt(String input, String b64key) throws Exception {
+    byte[] inArr = Base64.decode(input, Base64.DEFAULT);
+    byte[] pubKey = Base64.decode(b64key, Base64.DEFAULT);
+    Key publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(pubKey));
+    Cipher c = Cipher.getInstance("RSA");
+    c.init(Cipher.DECRYPT_MODE, publicKey);
+    byte[] decodedBytes = c.doFinal(inArr);
+    String output = new String(decodedBytes, "UTF8");
+
+    return output;
   }
 }
