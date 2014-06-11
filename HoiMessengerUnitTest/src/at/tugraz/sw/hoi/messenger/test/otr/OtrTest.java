8ff43fb0922c73a34fa02ed54858baf4d3e4a7e3
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2014-06-11 08:55:20 +0200

    [HB,CB]{test-before-code} OTR classes

--- HoiMessengerUnitTest/src/at/tugraz/sw/hoi/messenger/test/otr/OtrTest.java
+++ HoiMessengerUnitTest/src/at/tugraz/sw/hoi/messenger/test/otr/OtrTest.java
@@ -9,11 +9,27 @@ import at.tugraz.sw.hoi.messenger.otr.HoiOtrEngineHost;
 
 public class OtrTest extends junit.framework.TestCase {
 
-	private SessionID aliceSessionID = new SessionID("Alice@Wonderland",
-			"Bob@Wonderland", "Scytale");
+	public void testSessionIdEquality() throws Exception {
+		SessionID aliceSessionID2 = getAliceSessionID();
+		assertTrue(getAliceSessionID().equals(aliceSessionID2));
+	}
 
-	private SessionID bobSessionID = new SessionID("Bob@Wonderland",
-			"Alice@Wonderland", "Scytale");
+	private SessionID getAliceSessionID() {
+		return new SessionID("Alice@Wonderland", "Bob@Wonderland", "Scytale");
+	}
+
+	private SessionID getBobSessionID() {
+		return new SessionID("Bob@Wonderland", "Alice@Wonderland", "Scytale");
+	}
+
+	private HoiOtrEngineHost getHostAlice() {
+		return new HoiOtrEngineHost(new OtrPolicyImpl(OtrPolicy.ALLOW_V2
+				| OtrPolicy.ERROR_START_AKE));
+	}
+
+	private HoiOtrEngine getUsAlice(HoiOtrEngineHost host) {
+		return new HoiOtrEngine(host);
+	}
 
 	public void testSession() throws Exception {
 		this.startSession();
@@ -27,44 +43,50 @@ public class OtrTest extends junit.framework.TestCase {
 	private HoiOtrEngineHost hostBob;
 
 	private void startSession() {
-		hostAlice = new HoiOtrEngineHost(new OtrPolicyImpl(OtrPolicy.ALLOW_V2
-				| OtrPolicy.ERROR_START_AKE));
+		hostAlice = getHostAlice();
 		hostBob = new HoiOtrEngineHost(new OtrPolicyImpl(OtrPolicy.ALLOW_V2
 				| OtrPolicy.ERROR_START_AKE));
 
 		usAlice = new HoiOtrEngine(hostAlice);
 		usBob = new HoiOtrEngine(hostBob);
 
-		usAlice.startSession(aliceSessionID);
+		usAlice.startSession(getAliceSessionID());
 
 		// Bob receives query, sends D-H commit.
-		usBob.transformReceiving(bobSessionID, hostAlice.lastInjectedMessage);
+		usBob.transformReceiving(getBobSessionID(),
+				hostAlice.lastInjectedMessage);
 
 		// Alice received D-H Commit, sends D-H key.
-		usAlice.transformReceiving(aliceSessionID, hostBob.lastInjectedMessage);
+		hostAlice = getHostAlice();
+		usAlice = getUsAlice(hostAlice); // = new HoiOtrEngine(hostAlice);
+		usAlice.transformReceiving(getAliceSessionID(),
+				hostBob.lastInjectedMessage);
 
 		// Bob receives D-H Key, sends reveal signature.
-		usBob.transformReceiving(bobSessionID, hostAlice.lastInjectedMessage);
+		usBob.transformReceiving(getBobSessionID(),
+				hostAlice.lastInjectedMessage);
 
 		// Alice receives Reveal Signature, sends signature and goes secure.
-		usAlice.transformReceiving(aliceSessionID, hostBob.lastInjectedMessage);
+		usAlice.transformReceiving(getAliceSessionID(),
+				hostBob.lastInjectedMessage);
 
 		// Bobs receives Signature, goes secure.
-		usBob.transformReceiving(bobSessionID, hostAlice.lastInjectedMessage);
+		usBob.transformReceiving(getBobSessionID(),
+				hostAlice.lastInjectedMessage);
 
-		if (usBob.getSessionStatus(bobSessionID) != SessionStatus.ENCRYPTED
-				|| usAlice.getSessionStatus(aliceSessionID) != SessionStatus.ENCRYPTED)
+		if (usBob.getSessionStatus(getBobSessionID()) != SessionStatus.ENCRYPTED
+				|| usAlice.getSessionStatus(getAliceSessionID()) != SessionStatus.ENCRYPTED)
 			fail("Could not establish a secure session.");
 	}
 
 	private void exchageMessages() {
 		// We are both secure, send encrypted message.
 		String clearTextMessage = "Hello Bob, this new IM software you installed on my PC the other day says we are talking Off-the-Record, what is that supposed to mean?";
-		String sentMessage = usAlice.transformSending(aliceSessionID,
+		String sentMessage = usAlice.transformSending(getAliceSessionID(),
 				clearTextMessage);
 
 		// Receive encrypted message.
-		String receivedMessage = usBob.transformReceiving(bobSessionID,
+		String receivedMessage = usBob.transformReceiving(getBobSessionID(),
 				sentMessage);
 
 		if (!clearTextMessage.equals(receivedMessage))
@@ -72,10 +94,11 @@ public class OtrTest extends junit.framework.TestCase {
 
 		// Send encrypted message.
 		clearTextMessage = "Hey Alice, it means that our communication is encrypted and authenticated.";
-		sentMessage = usBob.transformSending(bobSessionID, clearTextMessage);
+		sentMessage = usBob.transformSending(getBobSessionID(),
+				clearTextMessage);
 
 		// Receive encrypted message.
-		receivedMessage = usAlice.transformReceiving(aliceSessionID,
+		receivedMessage = usAlice.transformReceiving(getAliceSessionID(),
 				sentMessage);
 
 		if (!clearTextMessage.equals(receivedMessage))
@@ -83,20 +106,22 @@ public class OtrTest extends junit.framework.TestCase {
 
 		// Send encrypted message.
 		clearTextMessage = "Oh, is that all?";
-		sentMessage = usAlice
-				.transformSending(aliceSessionID, clearTextMessage);
+		sentMessage = usAlice.transformSending(getAliceSessionID(),
+				clearTextMessage);
 
 		// Receive encrypted message.
-		receivedMessage = usBob.transformReceiving(bobSessionID, sentMessage);
+		receivedMessage = usBob.transformReceiving(getBobSessionID(),
+				sentMessage);
 		if (!clearTextMessage.equals(receivedMessage))
 			fail();
 
 		// Send encrypted message.
 		clearTextMessage = "Actually no, our communication has the properties of perfect forward secrecy and deniable authentication.";
-		sentMessage = usBob.transformSending(bobSessionID, clearTextMessage);
+		sentMessage = usBob.transformSending(getBobSessionID(),
+				clearTextMessage);
 
 		// Receive encrypted message.
-		receivedMessage = usAlice.transformReceiving(aliceSessionID,
+		receivedMessage = usAlice.transformReceiving(getAliceSessionID(),
 				sentMessage);
 
 		if (!clearTextMessage.equals(receivedMessage))
@@ -105,21 +130,22 @@ public class OtrTest extends junit.framework.TestCase {
 		// Send encrypted message. Test UTF-8 space characters.
 		clearTextMessage = "Oh really?! pouvons-nous parler en français?";
 
-		sentMessage = usAlice
-				.transformSending(aliceSessionID, clearTextMessage);
+		sentMessage = usAlice.transformSending(getAliceSessionID(),
+				clearTextMessage);
 
 		// Receive encrypted message.
-		receivedMessage = usBob.transformReceiving(bobSessionID, sentMessage);
+		receivedMessage = usBob.transformReceiving(getBobSessionID(),
+				sentMessage);
 		if (!clearTextMessage.equals(receivedMessage))
 			fail();
 	}
 
 	private void endSession() {
-		usBob.endSession(bobSessionID);
-		usAlice.endSession(aliceSessionID);
+		usBob.endSession(getBobSessionID());
+		usAlice.endSession(getAliceSessionID());
 
-		if (usBob.getSessionStatus(bobSessionID) != SessionStatus.PLAINTEXT
-				|| usAlice.getSessionStatus(aliceSessionID) != SessionStatus.PLAINTEXT)
+		if (usBob.getSessionStatus(getBobSessionID()) != SessionStatus.PLAINTEXT
+				|| usAlice.getSessionStatus(getAliceSessionID()) != SessionStatus.PLAINTEXT)
 			fail("Failed to end session.");
 	}
 }
